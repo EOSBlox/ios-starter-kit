45c9dd5cc035eb514e7b9d5a56aca281a78d936f
Will in China <email.will.in.china@gmail.com>
2018-08-15 21:08:03 +0800

    basic blox app for IOS

---
+++ README.md
@@ -0,0 +1,32 @@
+# EOS Blox IOS Starter Kit
+
+This template is a starting point for building EOS Blox apps for IOS.
+
+### Setup
+
+##### Prerequisites
+
+Install [Polymer CLI](https://github.com/Polymer/polymer-cli) using
+[npm](https://www.npmjs.com) (we assume you have pre-installed [node.js](https://nodejs.org)).
+
+    npm install -g polymer-cli@next
+
+Install [Cordova CLI](https://cordova.apache.org/docs/en/8.x/guide/cli/index.html)
+
+    sudo npm install -g cordova
+
+Check you have all prerequisiites for [IOS](https://cordova.apache.org/docs/en/8.x/guide/platforms/ios/index.html#requirements-and-support)
+
+    cordova requirements
+
+### Build
+
+    npm run build
+
+### Serve in Browser
+
+    npm run serve
+
+### Serve in Emulator
+
+    npm run emulate
---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,54 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-webview.IonicWebView",
+    "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
+    "pluginId": "cordova-plugin-ionic-webview",
+    "clobbers": [
+      "Ionic.WebView"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+    "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+    "pluginId": "cordova-plugin-ionic-webview",
+    "clobbers": [
+      "cordova.exec"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-keyboard.keyboard",
+    "file": "plugins/cordova-plugin-ionic-keyboard/www/ios/keyboard.js",
+    "pluginId": "cordova-plugin-ionic-keyboard",
+    "clobbers": [
+      "window.Keyboard"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-device": "2.0.2",
+  "cordova-plugin-splashscreen": "5.0.2",
+  "cordova-plugin-ionic-webview": "2.0.3",
+  "cordova-plugin-ionic-keyboard": "2.1.2"
+};
+// BOTTOM OF METADATA
+});
