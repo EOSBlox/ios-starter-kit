5c9d46a8e5419a1b47933487fa8e504551d3f136
Will in China <email.will.in.china@gmail.com>
2018-08-16 23:33:20 +0800

    styles

---
+++ src/shared-styles.js
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+import '@polymer/polymer/polymer-element.js';
+
+const $_documentContainer = document.createElement('template');
+$_documentContainer.innerHTML = `<dom-module id="shared-styles">
+  <template>
+    <style>
+      :host {
+        --header-bg-color: #FFFFFF;
+        --header-color: #62676F;
+        --border-color-1: #EBEBEB;
+        --app-primary-color: #4285f4;
+        --app-secondary-color: black;
+        display: block;
+      }
+      h2 {
+        padding: 30px 20px 8px;
+        font-size: 26px;
+        margin:0;
+      }
+    </style>
+  </template>
+</dom-module>`;
+
+document.head.appendChild($_documentContainer.content);
